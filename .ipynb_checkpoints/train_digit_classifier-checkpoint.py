ed5a2cfe1754e95fb5f4f97e61c79440e882dcba
TheNoobInventor <eduobijoro@mailfence.com>
2022-09-18 00:12:58 +0100

    Minor directory and file changes

--- .ipynb_checkpoints/train_digit_classifier-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from keras.datasets import mnist
-from keras.utils.np_utils import to_categorical
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
-
-# Load MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
